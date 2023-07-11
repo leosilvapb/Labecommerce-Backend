cb8004f3e2228af37ee1a22a64a9f9460947d0e5
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-07-11 14:36:51 -0300

    aprofundamento em sql comcluido

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -9,10 +9,6 @@ CREATE TABLE
         created_at TEXT NOT NULL
     );
 
-SELECT * FROM users;
-
-PRAGMA table_info('users');
-
 INSERT INTO
     users(
         id,
@@ -26,22 +22,20 @@ VALUES (
         'Fulano',
         'fulano@email.com',
         'fulano123',
-        DATE('now')
+        DATETIME('now', 'localtime')
     ), (
         'u002',
         'Beltrana',
         'beltrana@email.com',
         'beltrana00',
-        DATE('now')
+        DATETIME('now', 'localtime')
     ), (
-        "u003",
-        "leonardo",
-        "leo@email.com",
-        "123456",
-        DATE('now')
+        'u003',
+        'leonardo',
+        'leo@email.com',
+        '123456',
+        DATETIME('now', 'localtime')
     );
-
-DELETE FROM users WHERE id = "u001";
 
 CREATE TABLE
     products(
@@ -52,7 +46,67 @@ CREATE TABLE
         image_url TEXT NOT NULL
     );
 
+INSERT INTO
+    products(
+        id,
+        name,
+        price,
+        description,
+        image_url
+    )
+VALUES (
+        'prod001',
+        'Mouse gamer',
+        250,
+        'Melhor mouse do mercado!',
+        'https://picsum.photos/seed/Monitor/400'
+    ), (
+        'prod002',
+        'Monitor',
+        900,
+        'Monitor LED Full HD 24 polegadas',
+        'https://picsum.photos/seed/Monitor/400'
+    ), (
+        'prod003',
+        'SSD gamer',
+        349.99,
+        'Acelere seu sistema com velocidades incríveis de leitura e gravação.',
+        'https://picsum.photos/seed/Monitor/400'
+    ), (
+        'prod004',
+        'Placa de vídeo RTX 4080',
+        8200,
+        'Melhore o desempenho do seu PC em jogos com a nova placa RTX 4080',
+        'https://picsum.photos/seed/Monitor/400'
+    ), (
+        'prod005',
+        'Placa de vídeo RTX 3060TI',
+        2300,
+        'Melhore o desempenho do seu PC em jogos.',
+        'https://picsum.photos/seed/Monitor/400'
+    );
+
+SELECT * FROM users;
+
 SELECT * FROM products;
+
+SELECT * FROM products WHERE name LIKE '%gamer%';
+
+INSERT INTO
+    users(
+        id,
+        name,
+        email,
+        password,
+        created_at
+    )
+VALUES (
+        'u004',
+        'Amanda',
+        'amanda@email.com',
+        'amanda123',
+        DATETIME('now', 'localtime')
+    );
 
 INSERT INTO
     products(
@@ -63,33 +117,27 @@ INSERT INTO
         image_url
     )
 VALUES (
-        "prod001",
-        "Mouse gamer",
-        250,
-        "Melhor mouse do mercado!",
-        "https://picsum.photos/seed/Monitor/400"
-    ), (
-        "prod002",
-        "Monitor",
-        900,
-        "Monitor LED Full HD 24 polegadas",
-        "https://picsum.photos/seed/Monitor/400"
-    ), (
-        "prod003",
-        "SSD gamer",
-        349.99,
-        "Acelere seu sistema com velocidades incríveis de leitura e gravação.",
-        "https://picsum.photos/seed/Monitor/400"
-    ), (
-        "prod004",
-        "Placa de vídeo RTX 4080",
-        8200,
-        "Melhore o desempenho do seu PC em jogos com a nova placa RTX 4080",
-        "https://picsum.photos/seed/Monitor/400"
-    ), (
-        "prod005",
-        "Placa de vídeo RTX 3060TI",
-        2300,
-        "Melhore o desempenho do seu PC em jogos.",
-        "https://picsum.photos/seed/Monitor/400"
-    )
+        'prod006',
+        'Headset Gamer Redragon Zeus X H510',
+        248,
+        'O Headset Gamer Zeus X rgb conta com iluminação Chroma Mk. Com qualidade e som surround 7.1!',
+        'https://picsum.photos/seed/Monitor/400'
+    );
+
+DELETE FROM users WHERE id = 'u004';
+
+SELECT * FROM users;
+
+DELETE FROM products WHERE id = 'prod006';
+
+SELECT * FROM products;
+
+UPDATE products
+SET
+    name = 'Headset Gamer Redragon Zeus X',
+    price = 300,
+    description = 'O Headset Gamer Zeus X rgb conta com qualidade e som surround 7.1!',
+    image_url = 'https://picsum.photos/200/300'
+WHERE id = 'prod006';
+
+DROP TABLE users;
