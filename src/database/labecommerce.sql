a02f8c372e2e1b2628f1f7df7e329bd9f187463b
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-07-12 14:28:51 -0300

    relações SQL I finalizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -141,3 +141,63 @@ SET
 WHERE id = 'prod006';
 
 DROP TABLE users;
+
+CREATE TABLE
+    purchases(
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        buyer TEXT NOT NULL,
+        total_price REAL NOT NULL,
+        created_at TEXT NOT NULL,
+        FOREIGN KEY(buyer) REFERENCES users(id)
+    );
+
+INSERT INTO
+    purchases(
+        id,
+        buyer,
+        total_price,
+        created_at
+    )
+VALUES (
+        'purch001',
+        'u004',
+        8200,
+        DATETIME('now', 'localtime')
+    ), (
+        'purch002',
+        'u003',
+        3200,
+        DATETIME('now', 'localtime')
+    ), (
+        'purch003',
+        'u001',
+        4500,
+        DATETIME('now', 'localtime')
+    ), (
+        'purch004',
+        'u002',
+        1850,
+        DATETIME('now', 'localtime')
+    );
+
+SELECT * FROM purchases;
+
+UPDATE purchases SET total_price = 8099 WHERE id = 'purch001';
+
+UPDATE purchases SET total_price = 3300 WHERE id ='purch002';
+
+UPDATE purchases SET total_price = 4550 WHERE id ='purch003';
+
+UPDATE purchases SET total_price = 2050 WHERE id ='purch004';
+
+DROP TABLE purchases;
+
+SELECT
+    purchases.id AS idPedido,
+    purchases.buyer AS idCliente,
+    users.name AS cliente,
+    users.email,
+    purchases.total_price AS precoTotal,
+    purchases.created_at AS dataCompra
+FROM purchases
+    INNER JOIN users ON purchases.buyer = users.id;
